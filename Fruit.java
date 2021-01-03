3a465ae3a055a71ea5e474437a03eda2d6db4174
antoni0-x <antoni0-x@mail.ru>
2021-01-03 21:23:04 +0300

    домашнее задание

---
+++ Fruit.java
@@ -0,0 +1,14 @@
+package javaProfi.lesson1;
+
+public class Fruit {
+    private float weight;
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+}
